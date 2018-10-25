bb2dd2fd73bf28b1e09b223fb655e522221a20f0
TroyG <gordontaaj@gmail.com>
2018-10-24 19:50:35 -0700

    uploading readme and photos

---
+++ ReadMe.md
@@ -0,0 +1,61 @@
+# LIRI Bot
+
+### Overview
+
+LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a _Language_ Interpretation and Recognition Interface. LIRI is a command line node app that takes in parameters and gives you back data.
+
+### what it can do
+
+1. LIRI can display searched movie data concerts from your favorit artist and songs from spotify
+
+2. To retrieve the data that will power this app, I utilized the Twitter, Spotify and OMDB APIs.
+
+   * [Twitter](https://www.npmjs.com/package/twitter)
+   
+   * [Node-Spotify-API](https://www.npmjs.com/package/node-spotify-api)
+   
+   * [Request](https://www.npmjs.com/package/request)
+   * [OMDB API](http://www.omdbapi.com).
+
+   * [DotEnv](https://www.npmjs.com/package/dotenv)
+
+### Instructions
+
+1. `node liri.js concert-this <artist name here>'`
+
+   * This will show you concerts from that artis in your terminal/bash window.
+
+2. `node liri.js spotify-this-song '<song name here>'`
+
+   * This will show the following information about the song in your terminal/bash window
+     
+     * Artist(s)
+     
+     * The song's name
+     
+     * A preview link of the song from Spotify
+     
+     * The album that the song is from
+
+3. `node liri.js movie-this '<movie name here>'`
+
+   * This will output the following information to your terminal/bash window:
+
+     ```
+       * Title of the movie.
+       * Year the movie came out.
+       * IMDB Rating of the movie.
+       * Rotten Tomatoes Rating of the movie.
+       * Country where the movie was produced.
+       * Language of the movie.
+       * Plot of the movie.
+       * Actors in the movie.
+     ```
+
+4. `node liri.js do-what-it-says`
+   
+   * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+     
+     * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
+     
+     * Feel free to change the text in that document to test out the feature for other commands.
--- liri.js
+++ liri.js
@@ -32,6 +32,7 @@ var callSpotifyAPI = function(songName) {
       }
       var songs = data.tracks.items;
       for (var i = 0; i < songs.length; i++) {
+        console.log('-------------------------------------------------')
         console.log(i);
         console.log("Artist name: " + songs[i].artists[0].name);
         console.log("Song title: " + songs[i].name);
@@ -40,7 +41,8 @@ var callSpotifyAPI = function(songName) {
         console.log("Release date: " + songs[i].album.release_date);
         console.log("Album type: " + songs[i].album.album_type);
         console.log("Preview song: " + songs[i].preview_url);
-        console.log("----------------------------------------------------");
+        console.log('-------------------------------------------------')
+        
       }
     }
   );
@@ -57,6 +59,7 @@ var callOMDBAPI = function(movieName) {
   request(urlHit, function(error, response, body) {
     if (!error && response.statusCode === 200) {
       var jsonData = JSON.parse(body);
+      console.log('-------------------------------------------------')
       console.log("Title: " + jsonData.Title);
       console.log("Year: " + jsonData.Year);
       console.log("Rated: " + jsonData.Rated);
@@ -66,6 +69,7 @@ var callOMDBAPI = function(movieName) {
       console.log("Plot: " + jsonData.Plot);
       console.log("Actors: " + jsonData.Actors);
       console.log("Rotton Tomatoes Rating: " + jsonData.Ratings[1].Value);
+      console.log('-------------------------------------------------')
     }
   });
 };
@@ -78,24 +82,19 @@ var callBANDSAPI = function(artist) {
   var urlHit = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
   request(urlHit, function(error, response, body) {
     if (!error && response.statusCode === 200) {
-      console.log('body:', body);
       var data = JSON.parse(body);
       for (var i = 0; i < data.length; i++) {
-      console.log("Lineup: " + data[i].lineup)
+        console.log('-------------------------------------------------')
+        console.log("Lineup: " + data[i].lineup)
       console.log("Venue: " + data[i].venue.name);
        console.log("City: " + data[i].venue.city);
        console.log("Region: " + data[i].venue.region);
        console.log("Country: " + data[i].venue.country);
-       console.log("Type: " + data[i].offers.type);
-       console.log("status: " + data[i].offers.status);
        var date = data[i].datetime;
        var time = data[i].datetime;
        time = moment(time).format("h:mm a");
        date = moment(date).format("MM/DD/YYYY");
-       console.log("concert starts at: " + date +"  " +  time)
-       var sdate = data[i].on_sale_datetime;
-       sdate = moment(sdate).format("MM/DD/YYYY");
-       console.log("Sale started: "  + sdate)
+       console.log("concert starts at: " + date + "  " +  time)
     }
   }
 });
